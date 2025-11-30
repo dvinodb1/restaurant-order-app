5ca1b9595a6ac5803025027f6cee5ad1741d28a2
dvinodb1 <dvinodb1@gmail.com>
2025-11-30 21:01:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const MENU_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSgt2py8ONY_7lLQcVsVQcnCO7lgpeIWT1bzUcvK0Vc-95Ll-27YbUTRdgH4XnBw5HhT_7IjUpGEeav/pub?gid=1477636374&single=true&output=csv';
 
 // 🔗 Replace with your Google Apps Script Web App URL (from deployment)
-const ORDER_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbyKeV28LDjVIm_Gl9Sb45oC7Z5G1u0fFRbD0Ls8QCAOT4FeOa5o1MLv44BBGdPD8x0inw/exec';
+const ORDER_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbzi9aqTxdW73yWtluOPaxVal4lefuGr_VurJ1Z2qZPI-P6X6aoLOlNoyt-Q9iu_GcVj/exec';
 
 let MENU = [];
 let cart = {}; // { "Pizza": { qty: 2, price: 12 }, ... }
